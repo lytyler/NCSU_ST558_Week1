36fdf819b7d9ddce839fe5d48eb4ef5f365003d4
Lanette Tyler <lytyler@ncsu.edu>
2025-08-24 17:59:27 -0400

    adding pre-existing Week1.R file from RStudio

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # NCSU_ST558_Week1
 Cloning a repo and working in RStudio
 
-Adding a line from RStudio
+Adding a line from RStudio (to push with menus)
+Adding another line from RStudio (to push with terminal)
---
+++ Week1.R
@@ -0,0 +1,20 @@
+NCSU ST558 Week 1
+
+iris
+class(iris)
+typeof(iris)
+str(iris)
+summary(iris)
+is.vector(iris)
+is.data.frame(iris)
+
+PL <- iris$Petal.Length
+class(PL)
+typeof(PL)
+str(PL)
+summary(PL)
+is.vector(PL)
+is.data.frame(PL)
+
+getwd()
+setwd(" ")
