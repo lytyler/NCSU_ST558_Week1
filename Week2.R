7e6a5ba4b22064c05613b823b2ae00f593bdb328
Lanette Tyler <lytyler@ncsu.edu>
2025-08-30 12:11:08 -0400

    Updating github files after data frame additions

--- Week2.R
+++ Week2.R
@@ -1,6 +1,7 @@
 # NCSU 558 Week 2
-# Matrices
+# Matrices and Data Frames
 
+# Matrices-----------------------------------------------------------------------
 ## Create a matrix
 my_matrix <- matrix(c(1,3,4,-1,5,6),nrow=3,ncol=2)
 my_matrix
@@ -65,7 +66,7 @@ tail(my_iris)
 my_matrix3 <- matrix(c(runif(10),rnorm(10),rgamma(10,shape=1,scale=1)),nrow=10,dimnames=list(1:10,c("Uniform","Normal","Gamma")))
 my_matrix3
 
-# Accessing ELements of a matrix:
+# Accessing Elements of a matrix:
 my_matrix4 <- matrix(c(1:4,20:17),ncol=2)
 my_matrix4
 my_matrix4[2,2]
@@ -84,3 +85,61 @@ dim(my_matrix4)
 my_array <- array(1:24,dim=c(4,2,3))
 my_array
 my_array[3,1,2]
+
+
+# Data Frames -------------------------------------------------------------------
+# Create data frame:
+# First make vectors, then combine them into data frame
+
+# first vector
+x <- c("a","b","c","d","e","f","g")
+x
+length(x)
+str(x)
+
+# second vector
+y <- c(1,2,3,-4,5,6,7)
+y
+length(y)
+str(y)
+
+# third vector
+z <- 10:16
+z
+length(z)
+str(z)
+
+# combine vectors into data frame
+my_df <- data.frame(x,y,z)
+my_df
+
+# data frame attributes
+str(my_df)
+attributes(my_df)
+
+# default names (taken from original objects passed in, and nonsense for columns, rows seem to be numbered)
+data.frame(1:5,c("a","b","c","d","e"))
+
+#can set column names explicitly:
+my_df <- data.frame(char=x,data1=y,data2=z)
+my_df
+# another way to set column names explicitly at data frame creation:
+data.frame(number=1:5,letter=c("a","b","c","d","e"))
+
+#Accessing elements in a data frame
+str(iris)
+iris[1,4]
+iris
+iris[1:4,2:4]
+iris[1,]
+iris[1:10,1] # R "drops this response down to a vector
+iris[1:10,1,drop=FALSE] #stop the drop from data frame to vector
+'['(iris,1:10,1,drop=FALSE) # another way to call the function
+
+# data frames usually have meaningful column names
+# we can use these to subset
+iris[1:5,c("Sepal.Length","Species")]
+
+# alternative way to access a column
+iris$Sepal.Length
+iris[,1] #same results
